4fef6405c55887a0b606ad15844e2d07c0304058
andresmarpz <andresmarpz@gmail.com>
2023-03-05 21:50:48 -0300

    create /settings page

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,9 @@
+import DefaultLayout from '~/components/layout/DefaultLayout'
+
+export default function Settings() {
+  return (
+    <DefaultLayout>
+      <h1>Settings</h1>
+    </DefaultLayout>
+  )
+}
