51e8027855a936ef2282f58429ab412a7956f8ea
andresmarpz <andresmarpz@gmail.com>
2023-03-05 21:50:17 -0300

    install tailwind-scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,5 +37,8 @@ module.exports = {
       }
     }
   },
-  plugins: [require('tailwindcss-animate')]
+  plugins: [
+    require('tailwindcss-animate'),
+    require('tailwind-scrollbar')({ nocompatible: true })
+  ]
 }
