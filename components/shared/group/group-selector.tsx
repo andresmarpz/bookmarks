ff94365a1a39d78791abe2d43a0e3008a3893f06
andresmarpz <andresmarpz@gmail.com>
2023-05-13 09:04:51 -0300

    comment group selector for now

--- components/shared/group/group-selector.tsx
+++ components/shared/group/group-selector.tsx
@@ -1,33 +1,29 @@
-'use client'
+"use client"
 
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
-  SelectValue
-} from '@/components/ui/select'
-import { Group } from '@/types/supabase'
+  SelectValue,
+} from "@/components/ui/select"
 
-interface Props {
-  groups: Group[]
-}
+export default function GroupSelector() {
+  // if (!groups.length) return null
 
-export default function GroupSelector({ groups }: Props) {
-  if (!groups.length) return null
-
-  return (
-    <Select>
-      <SelectTrigger className="flex items-center gap-2">
-        <SelectValue placeholder="Select a group.." />
-      </SelectTrigger>
-      <SelectContent>
-        {groups.map((group) => (
-          <SelectItem key={'select-' + group.slug} value={group.slug}>
-            {group.slug}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  )
+  // return (
+  //   <Select>
+  //     <SelectTrigger className="flex items-center gap-2">
+  //       <SelectValue placeholder="Select a group.." />
+  //     </SelectTrigger>
+  //     <SelectContent>
+  //       {groups.map((group) => (
+  //         <SelectItem key={'select-' + group.slug} value={group.slug}>
+  //           {group.slug}
+  //         </SelectItem>
+  //       ))}
+  //     </SelectContent>
+  //   </Select>
+  // )
+  return <div>asds</div>
 }
