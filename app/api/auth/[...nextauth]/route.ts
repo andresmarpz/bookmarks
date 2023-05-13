2e2c3d5546ff1962f566164b1ea2e2961303ed7a
andresmarpz <andresmarpz@gmail.com>
2023-05-13 09:31:29 -0300

    move next-auth to external file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,58 +1,6 @@
-import { prisma } from '@/lib/prisma'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { DefaultSession, NextAuthOptions, User } from 'next-auth'
-import NextAuth from 'next-auth/next'
-import GithubProvider from 'next-auth/providers/github'
+import NextAuth from "next-auth/next"
 
-declare module 'next-auth' {
-  interface Session extends DefaultSession {
-    user: {
-      id: number
-      uid: string
-      username?: string
-    } & DefaultSession['user']
-  }
-
-  interface User {
-    username?: string
-  }
-}
-
-export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: 'jwt'
-  },
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_SECRET!
-    })
-  ],
-  callbacks: {
-    session: async ({ session, token, user }) => {
-      if (!session?.user) return session
-
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          id: token.id,
-          uid: token.uid,
-          username: user?.username
-        }
-      }
-    },
-    jwt: async ({ user, token }) => {
-      if (user) {
-        const u = user as User & { uid: string }
-        token.id = Number(u.id)
-        token.uid = u.uid
-      }
-      return token
-    }
-  }
-}
+import { authOptions } from "@/lib/next-auth"
 
 const handler = NextAuth(authOptions)
 
