ae59df6a9e05fdf35cfad65ea144dffff266277b
andresmarpz <andresmarpz@gmail.com>
2023-07-21 21:56:15 -0300

    feat: add server action to delete a bookmark

---
+++ lib/actions/bookmark/delete-bookmark.ts
@@ -0,0 +1,23 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+import { Bookmark, Group } from "@prisma/client"
+import { z } from "zod"
+
+import { prisma } from "@/lib/prisma"
+
+import { actionWithZod } from "../action-with-zod"
+
+export const deleteBookmark = actionWithZod(
+  z.object({
+    id: z.string(),
+    group: z.string(),
+  }),
+  async ({ id, group }) => {
+    await prisma.bookmark.delete({
+      where: { id },
+    })
+
+    revalidatePath("/app/" + group)
+  }
+)
