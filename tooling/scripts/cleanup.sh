6353ddf443d47df8df66ab520bd648de92a1c3f7
andresmarpz <andresmarpz@gmail.com>
2023-08-28 21:37:48 -0300

    create basic cleanup script

---
+++ tooling/scripts/cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# If you have weird errors on the repository, run this script first.
+# If you can't execute as sudo, either "chmod +x cleanup.sh" or run the commands manually.
+
+# Delete .next and node_modules directories
+rm -rf ../../.next
+rm -rf ../../node_modules
+
+# Clear yarn cache
+yarn cache clean
+
+# Install dependencies again
+yarn install
