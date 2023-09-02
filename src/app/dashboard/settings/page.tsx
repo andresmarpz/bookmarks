0270d6a57d7d1fefa0572a743a0cc1019a062765
andresmarpz <andresmarpz@gmail.com>
2023-09-01 21:14:41 -0300

    update settings page to include SettingsForm component

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,17 +1,15 @@
 import { getSession } from "@/lib/auth/get-session"
+import { getUser } from "@/lib/query/user.queries"
 import DashboardPage from "@/components/pages/dashboard/DashboardPage"
+import SettingsForm from "@/components/pages/dashboard/settings/SetingsForm"
 
 export default async function SettingsPage() {
   const session = await getSession()
-  console.log(session)
+  const user = await getUser(session?.user.id)
 
   return (
     <DashboardPage title="Settings">
-      {/* <SettingForm name="Username"
-        description="Your unique @username on the site."
-        property="username"
-        action={updateUserUsername}
-      /> */}
+      <SettingsForm user={user!} />
     </DashboardPage>
   )
 }
