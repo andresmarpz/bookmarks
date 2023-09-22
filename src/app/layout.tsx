e2a06f7a006b54cdec3317f3853fc934a98a22c8
andresmarpz <andresmarpz@gmail.com>
2023-09-22 19:45:47 -0300

    remove surpressHydrationWarning prop on html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body className={cn(inter.className, calSans.variable, "bg-black")}>
         <Providers>{children}</Providers>
       </body>
