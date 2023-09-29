ab6519073ed2cb153260f91c67cf1efccbce5af8
andresmarpz <andresmarpz@gmail.com>
2023-09-29 20:07:13 -0300

    update auth middleware from next-auth to supabase

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,12 @@
-export { default } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+  await supabase.auth.getSession()
+  return res
+}
 
 export const config = { matcher: ["/dashboard/:path*"] }
