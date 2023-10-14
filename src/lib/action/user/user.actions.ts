f31e16c32dd7526d3d415c01a539d458f788d521
andresmarpz <andresmarpz@gmail.com>
2023-10-14 15:53:14 -0300

    update User actions & schemas to use the repository

--- src/lib/action/user/user.actions.ts
+++ src/lib/action/user/user.actions.ts
@@ -1,27 +1,82 @@
 "use server"
 
 import { revalidatePath } from "next/cache"
+import { cookies } from "next/headers"
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs"
 import { createServerAction } from "nza"
 
 import { withAuth } from "@/lib/action/middleware/with-auth"
 import {
+  createUserWithGithubSchema,
+  createUserWithPasswordSchema,
   updateUserNameSchema,
   updateUserUsernameSchema,
 } from "@/lib/action/user/user.schema"
-import { prisma } from "@/lib/prisma"
+import { userRepository } from "@/lib/repository/user.repository"
+
+export const createUserWithPassword = createServerAction()
+  .input(createUserWithPasswordSchema)
+  .handler(async ({ username, email, password }) => {
+    const [[byEmail], [byUsername]] = await Promise.all([
+      userRepository.findByEmail({ email, provider: "password" }),
+      userRepository.findByUsername({ username }),
+    ])
+    if (byEmail) {
+      return {
+        error: {
+          message: "Email already exists.",
+        },
+      }
+    }
+
+    if (byUsername) {
+      return {
+        error: {
+          message: "Username already exists.",
+        },
+      }
+    }
+
+    const { data, error } = await createServerActionClient({ cookies }).auth.signUp({
+      email: email,
+      password: password,
+    })
+    if (error)
+      return {
+        error,
+      }
+
+    try {
+      await userRepository.insertOne({
+        id: data.user?.id!,
+        email: email,
+        provider: "password",
+        username: username,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+export const createUserWithGithub = createServerAction()
+  .input(createUserWithGithubSchema)
+  .handler(async (params) => {
+    const insert = await userRepository.insertOne({
+      id: params.id,
+      email: params.email,
+      provider: "github",
+    })
+
+    if (!insert) {
+      throw new Error("Failed to create user.")
+    }
+  })
 
 export const updateUserUsername = createServerAction()
   .input(updateUserUsernameSchema)
   .use(withAuth)
   .handler(async ({ username }, locals) => {
-    await prisma.user.update({
-      where: {
-        id: locals.session.user!.id,
-      },
-      data: {
-        username,
-      },
-    })
+    await userRepository.updateOne(locals.session.data.session?.user.id!, { username })
 
     revalidatePath("/dashboard/settings")
   })
@@ -30,14 +85,7 @@ export const updateUserName = createServerAction()
   .input(updateUserNameSchema)
   .use(withAuth)
   .handler(async ({ name }, locals) => {
-    await prisma.user.update({
-      where: {
-        id: locals.session.user!.id,
-      },
-      data: {
-        name,
-      },
-    })
+    await userRepository.updateOne(locals.session.data.session?.user.id!, { name })
 
     revalidatePath("/dashboard/settings")
   })
