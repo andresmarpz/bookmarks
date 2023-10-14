b984be31f66323cd914d4fcab4f7c3fe781a26ac
andresmarpz <andresmarpz@gmail.com>
2023-10-14 13:19:53 -0300

    generate base drizzle migration

--- drizzle/0000_absent_talisman.sql
+++ drizzle/0000_absent_talisman.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS "groups" (
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	"name" text NOT NULL,
 	"slug" text NOT NULL,
-	"userId" text NOT NULL,
+	"userId" uuid NOT NULL,
 	CONSTRAINT "groups_slug_userId_unique" UNIQUE("slug","userId")
 );
 --> statement-breakpoint
@@ -24,6 +24,10 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"id" uuid PRIMARY KEY NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
-	"sub" text NOT NULL,
-	"username" text
+	"username" text,
+	"email" text NOT NULL,
+	"provider" text NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username"),
+	CONSTRAINT "users_provider_email_unique" UNIQUE("provider","email"),
+	CONSTRAINT "users_id_email_unique" UNIQUE("id","email")
 );
