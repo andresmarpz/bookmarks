b7542232423c757c9c78f1d32b67b1515a0e3a31
andresmarpz <andresmarpz@gmail.com>
2023-10-14 15:52:37 -0300

    update withAuth action to use a supabase server action client instead

--- src/lib/action/middleware/with-auth.ts
+++ src/lib/action/middleware/with-auth.ts
@@ -1,7 +1,9 @@
-import { getSession } from "@/lib/auth/get-session"
+import { cookies } from "next/headers"
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs"
 
 export async function withAuth() {
-  const session = await getSession()
+  const supabase = createServerActionClient({ cookies })
+  const session = await supabase.auth.getSession()
 
   if (!session) throw new Error("You must be authenticated")
 
