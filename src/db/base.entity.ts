4ce93395671ce6827ffc5a37cbd7f660db29bd15
andresmarpz <andresmarpz@gmail.com>
2023-10-14 22:38:17 -0300

    make the base entity model ID default to a random uuid and be unique

--- src/db/base.entity.ts
+++ src/db/base.entity.ts
@@ -1,7 +1,7 @@
 import { timestamp, uuid } from "drizzle-orm/pg-core"
 
 export const baseEntity = {
-  id: uuid("id").primaryKey(),
+  id: uuid("id").defaultRandom().unique().primaryKey(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 }
