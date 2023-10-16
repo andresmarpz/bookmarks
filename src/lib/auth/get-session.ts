e41efdd2ba444ade9eb5a28d274aeeaa86bf9cf4
andresmarpz <andresmarpz@gmail.com>
2023-10-16 18:54:53 -0300

    update getSession to throw if not authed. ErrorBoundary should catch it

--- src/lib/auth/get-session.ts
+++ src/lib/auth/get-session.ts
@@ -1,14 +1,12 @@
 import { cache } from "react"
+import type { Session } from "@supabase/supabase-js"
 
 import { supabaseSCC } from "@/lib/supabase.rsc"
 
 export const getSession = cache(async () => {
   return supabaseSCC.auth.getSession().then((query) => {
-    return query.error
-      ? { error: query.error, session: null }
-      : {
-          session: query.data.session,
-          error: null,
-        }
+    if (query.error) throw query.error
+
+    return query.data.session as Session
   })
 })
