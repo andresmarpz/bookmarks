9fd650bc7c527fafc04d58f1123fff2de036baec
andresmarpz <andresmarpz@gmail.com>
2023-10-16 20:58:05 -0300

    make sure to properly redirect after an error on the OAuth callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -11,7 +11,13 @@ export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get("code")
 
-  let redirect = undefined
+  // URL to redirect to after sign in process completes
+  const returnTo = requestUrl.searchParams.get("returnTo")
+  const redirectUrl = request.nextUrl.clone()
+  redirectUrl.pathname = returnTo || "/dashboard"
+  redirectUrl.searchParams.delete("code")
+  redirectUrl.searchParams.delete("returnTo")
+
   if (code) {
     const supabase = createRouteHandlerClient({ cookies })
     const query = await supabase.auth.exchangeCodeForSession(code)
@@ -27,19 +33,13 @@ export async function GET(request: NextRequest) {
           id,
         })
 
-        redirect = "/auth/onboarding"
+        redirectUrl.pathname = "/auth/onboarding"
       } catch (err) {
-        return NextResponse.redirect("/auth/signin")
+        redirectUrl.pathname = "/auth/signin"
+        console.error(err)
+      } finally {
+        return NextResponse.redirect(redirectUrl)
       }
     }
   }
-
-  // URL to redirect to after sign in process completes
-  const returnTo = requestUrl.searchParams.get("returnTo")
-  const redirectUrl = request.nextUrl.clone()
-  redirectUrl.pathname = redirect || returnTo || "/dashboard"
-  redirectUrl.searchParams.delete("code")
-  redirectUrl.searchParams.delete("returnTo")
-
-  return NextResponse.redirect(redirectUrl)
 }
