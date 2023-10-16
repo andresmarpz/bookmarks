eae72f2290646a6ec77644f2f1f1aff410289936
andresmarpz <andresmarpz@gmail.com>
2023-10-16 18:52:13 -0300

    create Sign Up page

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,10 @@
+import AuthForm from "@/components/pages/auth/auth-form"
+
+export default async function SignUpPage() {
+  return (
+    <main className="w-full max-w-sm p-5">
+      <h1 className="text-xl">Sign Up</h1>
+      <AuthForm type="signup" />
+    </main>
+  )
+}
