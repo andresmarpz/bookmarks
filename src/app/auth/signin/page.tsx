8296ba931734c99ffc5871fdef4a07da41c76879
andresmarpz <andresmarpz@gmail.com>
2023-10-16 18:53:46 -0300

    update Sign In page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -7,9 +7,13 @@ export default async function LoginPage() {
     <main className="w-full max-w-sm p-5">
       <h1 className="text-xl">Sign In</h1>
       <h3 className="mb-6 text-sm text-neutral-400">
-        Don&apos;t have an account? <Link href="/auth/signup">Sign up</Link> now.
+        Don&apos;t have an account?{" "}
+        <Link href="/auth/signup" className="underline underline-offset-2">
+          Sign up
+        </Link>{" "}
+        now.
       </h3>
-      <AuthForm />
+      <AuthForm type="signin" />
     </main>
   )
 }
