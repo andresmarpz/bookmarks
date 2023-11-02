08c33bc4af993e39ef0f8469a5533ae59c6888db
andresmarpz <andresmarpz@gmail.com>
2023-11-02 18:22:46 -0300

    only validate env variables on Vercel

--- src/config/env.ts
+++ src/config/env.ts
@@ -8,6 +8,7 @@ configDotenv({
 const clientSchema = z.object({
   NEXT_PUBLIC_SUPABASE_URL: z.string().nonempty(),
   NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().nonempty(),
+  NEXT_PUBLIC_URL: z.string().nonempty(),
 })
 
 const serverSchema = z.object({
@@ -15,27 +16,46 @@ const serverSchema = z.object({
 })
 
 function parseEnvironment() {
+  type Env = {
+    client: z.infer<typeof clientSchema>
+    server?: z.infer<typeof serverSchema>
+  }
+
+  // If running outside of Vercel (e.g. Github Actions), parsing or throwing at
+  // the action "run time" is the same thing
+  if (!process.env.VERCEL) {
+    return {
+      client: clientSchema.safeParse(process.env),
+      server: serverSchema.safeParse(process.env),
+    } as unknown as Env
+  }
+
   const env = process.env
 
-  if (global.window !== undefined) {
-    const client = clientSchema
-      .refine((data) => {
-        for (const key in data) {
-          if (!key.startsWith("NEXT_PUBLIC_")) {
-            throw new Error(
-              `Invalid client environment variable, needs 'NEXT_PUBLIC_' prefix: ${key}`
-            )
-          }
+  const client = clientSchema
+    .refine((data) => {
+      for (const key in data) {
+        if (!key.startsWith("NEXT_PUBLIC_")) {
+          throw new Error(
+            `Invalid client environment variable, needs 'NEXT_PUBLIC_' prefix: ${key}`
+          )
         }
-        return true
-      })
-      .parse(env)
+      }
+      return true
+    })
+    .parse(env)
 
-    return { client }
-  } else {
+  // If we're on the server, parse the server environment variables
+  if (global.window === undefined) {
     const server = serverSchema.parse(env)
-    return { server }
+
+    return {
+      client,
+      server,
+    }
   }
+
+  return { client }
 }
 
 export const env = parseEnvironment()
