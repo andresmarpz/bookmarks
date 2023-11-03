0ff7de461370a012d434e4f02bf0ab486023b938
andresmarpz <andresmarpz@gmail.com>
2023-11-02 21:23:03 -0300

    prevent accessing the dashboard without a session, or the auth page if already logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,18 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
   const sessionQuery = await supabase.auth.getSession()
+  const loggedIn = sessionQuery.error || !sessionQuery.data.session
 
-  if (sessionQuery.error || !sessionQuery.data.session) {
-    const url = req.nextUrl.clone()
+  const url = req.nextUrl.clone()
+
+  if (!loggedIn && req.nextUrl.pathname.startsWith("/dashboard")) {
     url.pathname = "/auth/signin"
     url.searchParams.set("returnTo", req.nextUrl.pathname)
+
+    return NextResponse.redirect(url)
+  } else if (loggedIn && req.nextUrl.pathname.startsWith("/auth")) {
+    url.pathname = "/dashboard"
+    url.searchParams.delete("returnTo")
 
     return NextResponse.redirect(url)
   }
@@ -18,4 +25,4 @@ export async function middleware(req: NextRequest) {
   return res
 }
 
-export const config = { matcher: ["/dashboard/:path*"] }
+export const config = { matcher: ["/dashboard/:path*", "/auth/:path*"] }
